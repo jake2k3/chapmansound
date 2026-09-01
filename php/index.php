faf05016bd21fe5c2070a9502e0366883a9d8b9f
Jacob Chapman <jacob@chapmansound.com>
2026-09-01 10:31:56 -0500

    Logo sizing

--- php/index.php
+++ php/index.php
@@ -53,11 +53,11 @@
 		</div>
 
 		<div class="links-grid">
-			<a href='https://www.github.com/jake2k3' target='_blank' class="big-links badge-logo">
-				<img src='/php/images/logo-github.png' alt='My GitHub Profile'>
+			<a href='https://www.github.com/jake2k3' target='_blank' class="big-links">
+				<img src='/php/images/logo-github.png' class="badge-logo" alt='My GitHub Profile'>
 			</a>
-			<a href='https://www.linkedin.com/in/jacob-chapman-30a60a60/' target='_blank' class="big-links badge-logo">
-				<img src='/php/images/logo-linkedin.png' alt='My LinkedIn Profile'>
+			<a href='https://www.linkedin.com/in/jacob-chapman-30a60a60/' target='_blank' class="big-links">
+				<img src='/php/images/logo-linkedin.png' class="badge-logo" alt='My LinkedIn Profile'>
 			</a>
 		</div>
 
